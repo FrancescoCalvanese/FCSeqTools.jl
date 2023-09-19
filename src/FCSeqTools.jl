27e29785dc98368c3af3280e56487361842b91f7
FrancescoCalvanese <95098959+FrancescoCalvanese@users.noreply.github.com>
2023-09-19 22:07:44 +0200

    Update FCSeqTools.jl

--- src/FCSeqTools.jl
+++ src/FCSeqTools.jl
@@ -15,6 +15,9 @@ f=open(filename)
     lines=readlines(f)
     letter_matrix=Array{Char,2}(undef,Int64(1),Int64(0))
     for i in 1:Int64(length(lines))
+     while lines[i][1]==''
+	i=i+1
+     end
       if lines[i][1]=='>'
         j=1
         temp=join(lines[i+j])
